eadd197871ccb250a9cee9adb77ec92096f3f0ee
beingfastian <hi50903040@gmail.com>
2026-02-23 16:46:39 +0500

    Playing Instructions on real time

--- backend/database.py
+++ backend/database.py
@@ -5,18 +5,20 @@ from sqlalchemy.orm import sessionmaker
 from datetime import datetime
 from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
+load_dotenv(override=True)
 
-# Database URL from environment or default
+
 DATABASE_URL = os.getenv(
     "DATABASE_URL",
-    "postgresql://postgres:postgres@localhost:5432/audio_instructions"
+    "postgresql+psycopg://postgres:1234@localhost:5432/audio_instructions"
 )
+print("DATABASE_URL =", DATABASE_URL)
 
-# Create engine
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
 
 
 # Models
